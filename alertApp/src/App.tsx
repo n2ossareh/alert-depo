bf7fc19af792d4f62cde7cac0ad98ca23758cd01
Na Zo <98631994+n2ossareh@users.noreply.github.com>
2026-03-15 16:21:27 +0000

    completed app

---
+++ alertApp/src/App.tsx
@@ -0,0 +1,15 @@
+
+import Alert from './components/Alert'
+
+function App() {
+    return (
+      <div>
+        <Alert>
+          This <b>Succeeded</b>!
+        </Alert>
+      </div>
+    )
+}
+
+
+export default App
